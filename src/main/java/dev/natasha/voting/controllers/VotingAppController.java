109934e7514f1d25de35f108809202ba26b71e33
safety-panda <earmiffs+git@googlemail.com>
2022-02-10 16:13:04 +0000

    added navigation from welcome view to voting view

--- src/main/java/dev/natasha/voting/controllers/VotingAppController.java
+++ src/main/java/dev/natasha/voting/controllers/VotingAppController.java
@@ -2,8 +2,31 @@ package dev.natasha.voting.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import dev.natasha.voting.VotingApp;
+import dev.natasha.voting.navigator;
+
+import java.io.IOException;
 
 public class VotingAppController {
+    @FXML
+    private Label labelText;
+
+    @FXML
+    protected void onVoterButtonClick() throws IOException {
+        var stage = (Stage) labelText.getScene().getWindow();
+        stage.setScene(navigator.getScene("voting-view.fxml"));
+        stage.setTitle("Voting View");
+    }
+
+    @FXML
+    protected void onAuditorButtonClick() {}
+
+    @FXML
+    protected void onAdministratorButtonClick() {}
+
     @FXML
     private Label candidateSelectionText;
 
